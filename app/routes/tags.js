bf79aca42088431e3838179c43492974d500e6e8
std46d6b <dev@m5k.ru>
2022-08-02 20:38:48 +0300

    only unique tags

--- app/routes/tags.js
+++ app/routes/tags.js
@@ -5,13 +5,16 @@ import { postCreateValidation, postPatchValidation } from '../validations.js'
 import authCheck from '../utils/authCheck.js'
 import validate from '../utils/validationErrorsHandle.js'
 
+let uniq = (value, index, self) => { 
+    return self.indexOf(value) == index;
+}
 
 export default (app) => {
     app.get('/tags/last', async (req, res) => {
         try {
             let posts = await postModel.find().limit(5).exec()
 
-            let tags = posts.map(obj => obj.tags).flat().slice(0, 5)
+            let tags = posts.map(obj => obj.tags).flat().filter(uniq).slice(0, 5)
     
             res.json({
                 // ... user
