528ac728566349e8d4c4d0c19aec61963e57b43b
std46d6b <dev@m5k.ru>
2022-08-02 20:29:21 +0300

    2:34:37

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,10 +1,12 @@
 import auth from './auth.js'
 import post from './post.js'
+import tags from './tags.js'
 import storage from './storage.js'
 
 export default (app) => {
     auth(app)
     post(app)
+    tags(app)
     storage(app)
     
     console.log('[ + ] routes')
---
+++ app/routes/tags.js
@@ -0,0 +1,27 @@
+import config from '../config.js'
+
+import postModel from '../modeles/posts.js'
+import { postCreateValidation, postPatchValidation } from '../validations.js'
+import authCheck from '../utils/authCheck.js'
+import validate from '../utils/validationErrorsHandle.js'
+
+
+export default (app) => {
+    app.get('/tags/last', async (req, res) => {
+        try {
+            let posts = await postModel.find().limit(5).exec()
+
+            let tags = posts.map(obj => obj.tags).flat().slice(0, 5)
+    
+            res.json({
+                // ... user
+                status: 'succsess',
+                tags: tags
+            })
+        } catch (err) {
+            console.log(err)
+            return res.status(500).json({status: 'server error'})
+        }
+    })
+    console.log('      tags')
+}
